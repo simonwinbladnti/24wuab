3d9b8de7ff9075c126e3af8f67fbd7ee3fe70e37
simonwinbladnti <152278872+simonwinbladnti@users.noreply.github.com>
2024-02-21 14:51:14 +0100

    Vecka 8
    Start på landing page

---
+++ js/script.js
@@ -0,0 +1,37 @@
+let navButton = document.querySelector(".burger-menu");
+let menuActive = false;
+
+function showMenu() {
+    console.log("hej")
+    let menu = document.querySelector(".nav-container");
+    menu.classList.toggle("active");
+}
+
+navButton.addEventListener('click', showMenu);
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
